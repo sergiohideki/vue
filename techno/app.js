8d1f3094424386e30d34c9cfffb6a45a9f8a9e9d
Sergio Hideki <sergiohidekiono@gmail.com>
2020-01-05 16:40:32 -0300

    alerta

--- techno/app.js
+++ techno/app.js
@@ -3,7 +3,9 @@ const vm = new Vue({
     data: {
         produtos: [],
         produto: false,
-        carrinho: []
+        carrinho: [],
+        mensagemAlerta: "Item adicionado",
+        alertaAtivo: false
     },
     filters: {
         numeroPreco(valor) {
@@ -46,6 +48,7 @@ const vm = new Vue({
             this.produto.estoque--
             const { id, nome, preco } = this.produto //desestruturar
             this.carrinho.push({ id, nome, preco })
+            this.alerta(`${nome} adicionado ao carrinho.`)
         },
         removerItem(index) {
             this.carrinho.splice(index, 1)
@@ -54,6 +57,13 @@ const vm = new Vue({
             if (window.localStorage.carrinho) {
                 this.carrinho = JSON.parse(window.localStorage.carrinho)
             }
+        },
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem
+            this.alertaAtivo = true
+            setTimeout(() => {
+                this.alertaAtivo = false
+            }, 1500)
         }
     },
     watch: {
