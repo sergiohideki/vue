34935424d84ab83e18e5fab7848297f9437ccc71
Sergio Hideki <sergiohidekiono@gmail.com>
2020-01-15 20:29:48 -0300

    vue-async

---
+++ vue-async/src/App.vue
@@ -0,0 +1,45 @@
+<!--
+  Crie 3 componentes:
+
+  1: Componente falando sobre a empresa
+  2: Componente com os serviços da empresa
+  3: Componente com um formulário de contato
+
+  Crie um menu, que ao clique ele mude os componentes
+  acima de forma dinâmica. Não destrua o componente
+-->
+<template>
+  <div id="app">
+    <ul>
+      <li @click="abaAtiva = 'Formulario'">Formulário</li>
+      <li @click="abaAtiva = 'Servicos'">Serviço</li>
+      <li @click="abaAtiva = 'Sobre'">Sobre</li>
+    </ul>
+    <keep-alive>
+      <component :is="abaAtiva"></component>
+      <component :is="abaAtiva"></component>
+      <component :is="abaAtiva"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import Formulario from "./components/Formulario";
+// import Sobre from "./components/Sobre";
+
+export default {
+  name: "app",
+  components: {
+    Formulario,
+    Sobre: () => import("./components/Sobre") //transformando em async
+  },
+  data() {
+    return {
+      abaAtiva: "Formulario" // nome do componente que será trocado ao clicar no menu
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-async/src/components/Formulario.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{frase}}</div>
+</template>
+
+<script>
+export default {
+  name: "Formulario",
+  data() {
+    return {
+      frase: "sou o Formulario"
+    };
+  }
+};
+</script>
+
+<style></style>
