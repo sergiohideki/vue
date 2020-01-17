2a8119cb8257d212ab7420962e5ff9fa8dbe8c44
Sergio Hideki <sergiohidekiono@gmail.com>
2020-01-16 22:14:37 -0300

    animation-components

---
+++ animation-components/PaginaSobre.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Sobre da Empresa</h1>
+    <ul>
+      <li>Missão</li>
+      <li>Visão</li>
+      <li>Valores</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaginaSobre"
+};
+</script>
+
+<style>
+</style>
