28bea6d728bbbd5ddaf1a633cedf506e7e40aae1
Sergio Hideki <sergiohidekiono@gmail.com>
2020-01-20 11:01:05 -0300

    animation-lists

---
+++ animation-lists/list.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition-group tag="ul">
+    <li v-for="(produto, index) in carrinho" :key="produto.nome">
+      <p>{{produto.nome}}</p>
+      <p>{{produto.preco}}</p>
+      <button @click="remover(index)">Remover</button>
+    </li>
+  </transition-group>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      carrinho: [
+        { nome: "Notebook", preco: 2949 },
+        { nome: "Smartphone", preco: 3500 }
+      ]
+    };
+  },
+  methods: {
+    remover(index) {
+      this.carrinho.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+</style>
